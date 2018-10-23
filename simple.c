55f1fa265a7d7ff828886452f80ee189702e2db8
=Dinesh KC <=dineshkc@outlook.com>
2018-10-23 15:25:07 -0700

    v1

--- simple.c
+++ simple.c
@@ -1,5 +1,21 @@
 #include <stdio.h>
 
+char* read(){
+
+    char ch;
+    char* str;
+    str = malloc(10);
+    int i;
+    i = 0;
+    while(scanf(&ch)){
+        str[i] = ch;
+        i = i+1;
+    }
+}
+
 void main(){
+
+    char* str;
+    str = read();
     printf ("C is a cool program!");
 }
